4024b59535b7e2e0c1117b76f61dd53c6b59e379
Erick de Vargas Freire <freire.erick@escola.pr.gov.br>
2026-04-22 14:11:42 -0300

    second

---
+++ second.py
@@ -0,0 +1,23 @@
+def calculos (a, b):
+     a= int (input("valor 1: "))
+     b=  int (input("valor 2: "))
+     resultado = a + b
+     print (resultado)
+
+def userdata ():
+    username = input("insira nome:")
+    senha= input ("insira senha:")
+    
+    if username == "admin" and senha == "0000":
+        print ("acesso permitido")
+        calculos(0, 0)
+        
+    else:
+        print ("acesso negado")
+        
+def intro():
+    print ("\nBem vindo a super calculadora de elite\n")
+    
+intro()
+
+userdata()
